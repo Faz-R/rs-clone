b8a8d2717102e62bbbedc1c1bc8da3585a8e3fde
DrabantBY <drabant2022@gmail.com>
2023-01-31 13:23:55 +0300

    init: add base projects config

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <MainPage />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Products />,
+//         loader: loaderProductsList,
+//       },
+//       {
+//         path: 'products/:id',
+//         element: <ProductsItem />,
+//         loader: loaderProductsItem,
+//       },
+//     ],
+//   },
+// ]);
+
+// export const App = () => <RouterProvider router={router} />;
+
+const App = () => <h1>Hello Vite Team!</h1>;
+export default App;
