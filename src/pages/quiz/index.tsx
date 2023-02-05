0854a460aeb200d7ff2e2a792c532748601d9513
Faz-R <FazaR92@gmail.com>
2023-02-06 02:25:17 +0300

    feat: add all page functional

--- src/pages/quiz/index.tsx
+++ src/pages/quiz/index.tsx
@@ -1,4 +1,8 @@
+import { useEffect, useState } from 'react';
+import getMoviesData from '../../api/getMoviesData';
 import Button from '../../components/UI/button/Button';
+import Range from '../../components/UI/range/Range';
+import { MovieHumorInterface } from '../../types';
 import './index.scss';
 
 const Quiz = () => {
@@ -9,14 +13,71 @@ const Quiz = () => {
     '😊': ['детские', 'мультфильмы', 'семейные'],
     '🧐': ['исторические', 'спортивные', 'документальные', 'биографии'],
     '🥹': ['драмы', 'мелодрамы'],
-    '😯': ['ужасы', 'триллеры'],
-    '👻': ['аниме'],
+    '😱': ['ужасы', 'триллеры'],
+    '😻': ['аниме'],
+  };
+
+  const today = new Date();
+  const maxYear = today.getFullYear();
+  const minYear = 1920;
+
+  const [rate, setRate] = useState('');
+  const [minYearRange, setMinYear] = useState(minYear);
+  const [maxYearRange, setMaxYear] = useState(maxYear);
+
+  const rangeMinYear = (value: number) => {
+    setMinYear(value);
+  };
+
+  const rangeMaxYear = (value: number) => {
+    setMaxYear(value);
+  };
+
+  // const getMovie = async (e: any) => {
+  //   e.preventDefault();
+  //   const form = e?.target;
+  //   console.log(form);
+  //   // const formData = new FormData(form);
+  //   // console.log(formData);
+
+  //   // await getMoviesData(
+  //   //   {
+  //   //     genres: ,
+  //   //     year: `${minYearRange}-${maxYearRange}`,
+  //   //     rating:
+  //   //   },
+  //   //   true
+  //   // )
+  // };
+
+  const [movie, setMovie] = useState({} as MovieHumorInterface);
+
+  const getMovie: React.FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const formData = new FormData(form);
+    const rating = formData.get('rating') as string;
+    console.log(rating, 'rating');
+    const showViewed = formData.get('show') as string;
+    console.log(showViewed, 'showViewed');
+    const genres = (formData.get('emoji') as string).split(',');
+    console.log(genres, 'genres');
+    const minYearFrom = Number(formData.get('minYear'));
+    const maxYearFrom = Number(formData.get('maxYear'));
+    const year = `${Math.min(minYearFrom, maxYearFrom)}-${Math.max(minYearFrom, maxYearFrom)}`;
+    console.log(year, 'year');
+
+    const response = await getMoviesData({ genres, year, rating }, true);
+    console.log(response);
+    setMovie(response as MovieHumorInterface);
+    const movies = await response;
   };
 
   return (
     <section className="quiz">
-      <form className="emotions">
-        <div className="emotions__title">Выберите эмоцию</div>
+      <h3 className="quiz__title">Фильм по настроению</h3>
+      <form className="emotions" onSubmit={getMovie}>
+        <span className="emotions__title">Выберите эмоцию</span>
         <div className="emotions__list">
           {Object.entries(emotions).map(([key, value]) => {
             return (
@@ -28,12 +89,41 @@ const Quiz = () => {
                   id={`${key}`}
                   className="emoji__radio"
                   value={value}
+                  required
                 />
               </label>
             );
           })}
         </div>
-        <div className="emotions__title">Выберите год</div>
+        <span className="emotions__title">Выберите год</span>
+        <div className="range-block price-block">
+          <div className="range-values">
+            <p className="min-range"> {minYear}</p>
+            <p className="max-range">{maxYear}</p>
+          </div>
+          <Range
+            value={minYearRange}
+            min={minYear}
+            max={maxYear}
+            step={1}
+            onChange={rangeMinYear}
+            className="my-range min-range-slidebar"
+            name="minYear"
+          />
+          <Range
+            value={maxYearRange}
+            min={minYear}
+            max={maxYear}
+            step={1}
+            onChange={rangeMaxYear}
+            className="my-range max-range-slidebar"
+            name="maxYear"
+          />
+          <div>
+            {' '}
+            <strong>Year</strong>
+          </div>
+        </div>
         <div className="emotions__title">Выберите рейтинг</div>
         <label htmlFor="hight">
           Высокий
@@ -41,7 +131,7 @@ const Quiz = () => {
         </label>
         <label htmlFor="all">
           Неважно
-          <input type="radio" name="rating" id="all" value="" />
+          <input type="radio" name="rating" id="all" value="1-10" defaultChecked />
         </label>
         <div className="emotions__title">Убрать просмотренные и отложенные фильмы?</div>
         <label htmlFor="show">
@@ -50,11 +140,12 @@ const Quiz = () => {
         </label>
         <label htmlFor="hidden">
           Оставить
-          <input type="radio" name="show" id="hidden" value="false" />
+          <input type="radio" name="show" id="hidden" value="false" defaultChecked />
         </label>
 
         <Button>Отправить</Button>
       </form>
+      {movie && <div>{movie.name}</div>}
     </section>
   );
 };
