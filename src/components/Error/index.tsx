393bc27c965c478d8d734b0348052769f34b61b4
Faz-R <FazaR92@gmail.com>
2023-02-14 22:44:05 +0300

    feat: add design for error page

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -1,12 +1,20 @@
-import { useRouteError } from 'react-router-dom';
+import Button from '@components/UI/button/Button';
+import { Link, useRouteError } from 'react-router-dom';
+import classes from './index.module.scss';
 
 const Error = () => {
   const error = useRouteError() as Response;
   return (
-    <h2>
-      {error?.status ?? ''}
-      {error?.statusText ?? 'Something went wrong'}
-    </h2>
+    <div className={classes.error__wrapper}>
+      <div className={classes.error_page__subtitle}>
+        {error?.statusText ? `Ошибочка вышла!` : ''}
+      </div>
+      <div className={classes.error}>{error?.status ?? ''}</div>
+      <div className={classes.error_page__title}>{error?.statusText ?? 'Что-то пошло не так'}</div>
+      <Link to="/">
+        <Button>Вернуться на главную</Button>
+      </Link>
+    </div>
   );
 };
 
