daff835f78389a97b9be577de89697c502d47fcf
DrabantBY <drabant2022@gmail.com>
2023-02-19 14:54:56 +0300

    fix: import styles

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -1,4 +1,5 @@
 import { Player } from 'video-react';
+import '~video-react/dist/video-react.css';
 
 const VideoPlayer = ({ video, poster }: { video: string; poster: string }) => {
   return <Player playsInline poster={poster} src={video} />;
