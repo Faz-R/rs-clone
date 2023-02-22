e62c6dbc396d98f908b61aee1bc98c6021c680f3
Faz-R <FazaR92@gmail.com>
2023-02-23 00:34:34 +0300

    fix: fix type for parseMovieData

--- src/utils/getTrailer.ts
+++ src/utils/getTrailer.ts
@@ -1,9 +1,8 @@
 const getTrailer = (trailers?: { url: string }[]) => {
   if (!trailers) return null;
 
-  const result = trailers.find((trailer) =>
-    trailer.url.startsWith('https://www.youtube.com/')
-  )?.url;
+  const result =
+    trailers.find((trailer) => trailer.url.startsWith('https://www.youtube.com/'))?.url || null;
 
   return result;
 };
